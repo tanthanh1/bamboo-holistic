378ee921b54eaa85319d61d41956e279d2936068
Thanh <lygiathanh@gmail.com>
2024-09-30 22:25:49 +0700

    f

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/images/logo1.png";
 import { inter } from "@/assets/styles/fonts";
+import { useState } from "react";
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     return (
         <div>
             <nav class="bg-primary border-gray-200 dark:bg-gray-900 text-white  ">
@@ -28,7 +32,8 @@ const Navbar = () => {
                         type="button"
                         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-default"
-                        aria-expanded="false"
+                        aria-expanded={isMobileMenuOpen}
+                        onClick={() => setIsMobileMenuOpen((prev) => !prev)}
                     >
                         <span className="sr-only">Open main menu</span>
                         <svg
@@ -47,8 +52,9 @@ const Navbar = () => {
                             />
                         </svg>
                     </button>
+                    {/* Desktop Menu */}
                     <div
-                        class="hidden w-full md:block md:w-auto"
+                        className=" hidden w-full md:block md:w-auto"
                         id="navbar-default"
                     >
                         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -87,6 +93,50 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </div>
+
+                    {isMobileMenuOpen && (
+                        <div
+                            className=" w-full md:block md:w-auto"
+                            id="navbar-default"
+                        >
+                            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                <li>
+                                    <a
+                                        href="/"
+                                        className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-white "
+                                        aria-current="page"
+                                    >
+                                        Trang chủ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/service"
+                                        class="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    >
+                                        Dịch vụ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/about"
+                                        class="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    >
+                                        Giới thiệu
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="/contact"
+                                        className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    >
+                                        Liên hệ
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+
                     <div className="border-2 p-2 border-white ml-8  font-bold  hidden lg:block">
                         DT: 096 985 6512
                     </div>

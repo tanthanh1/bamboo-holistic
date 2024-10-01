ec8004ce7d550f3dbb6c8e95864b7d7faf12fdc8
Thanh <lygiathanh@gmail.com>
2024-10-01 21:21:11 +0700

    ##

--- utils/requests.js
+++ utils/requests.js
@@ -2,11 +2,7 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 async function fetchTickets() {
     try {
-        const res = await fetch(
-            `${apiDomain}/tickets`,
-            { cache: "no-store" },
-            { revalidate: 0 }
-        );
+        const res = await fetch(`${apiDomain}/tickets`, { cache: "no-store" });
         if (!res.ok) {
             throw new Error("Failed to fetch data");
         }

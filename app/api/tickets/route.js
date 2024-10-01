d454347312713ef435715a3ca340eec8067fa0c0
Thanh <lygiathanh@gmail.com>
2024-10-01 21:26:18 +0700

    Re

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -4,5 +4,13 @@ import Ticket from "@/models/Ticket";
 export const GET = async () => {
     await connectDB();
     const tickets = await Ticket.find({});
-    return new Response(JSON.stringify(tickets), { status: 200 });
+    return new Response(JSON.stringify(tickets), {
+        status: 200,
+        header: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store, no-cache, must-revalidate", // Ensures no caching
+            Pragma: "no-cache", // HTTP/1.0 compatibility
+            Expires: "0",
+        },
+    });
 };

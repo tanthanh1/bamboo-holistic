e8ab88010d4791e7a57b06149b4a139796099674
Thanh <lygiathanh@gmail.com>
2024-10-01 22:04:16 +0700

    Rr

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -6,7 +6,7 @@ export const GET = async () => {
     const tickets = await Ticket.find({});
     return new Response(JSON.stringify(tickets), {
         status: 200,
-        header: {
+        headers: {
             "Cache-Control": "public, s-maxage=1",
             "CDN-Cache-Control": "public, s-maxage=60",
             "Vercel-CDN-Cache-Control": "public, s-maxage=3600",

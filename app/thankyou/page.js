4f4f128077a1e13c712d07c44656fcddd6a87043
Thanh <lygiathanh@gmail.com>
2024-10-02 09:54:34 +0700

    Rr

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,7 +1,15 @@
 const ThankYouPage = () => {
     return (
-        <div>
-            Cám ơn bạn đã liên hệ. Chúng tôi sẽ gọi lại trong thời gian sớm nhất
+        <div className="bg-secondary text-xl text-center border border-2 m-16 px-16 py-8 rounded-md max-w-lg mx-auto">
+            <p>
+                Cám ơn bạn đã liên hệ. Chúng tôi sẽ gọi lại trong thời gian sớm
+                nhất
+            </p>
+            <p className="mt-4">
+                Trong trường hợp cần sự trợ giúp khẩn cấp, vui lòng liên hệ số
+                điện thoại:
+                <span className="font-bold"> 0969856512</span> (hoạt động 24/24){" "}
+            </p>
         </div>
     );
 };

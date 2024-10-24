dfa60c7f375a9d1972f6e38c637c664a833aa491
Thanh <lygiathanh@gmail.com>
2024-10-24 20:42:27 +0700

    Rrr

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     {
                         key: "Access-Control-Allow-Origin",
-                        value: "https://www.bambooholistic.vn",
+                        value: "https://bambooholistic.vn",
                     }, // replace this your actual origin
                     {
                         key: "Access-Control-Allow-Methods",

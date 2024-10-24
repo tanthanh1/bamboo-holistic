27d88c6f534eff99fbbefa6886dc0fec3ae621c5
Thanh <lygiathanh@gmail.com>
2024-10-24 20:30:07 +0700

    Rrr

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
                         key: "Access-Control-Allow-Headers",
                         value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
                     },
+                    {
+                        key: "Vary",
+                        value: "Origin",
+                    },
                 ],
             },
         ];

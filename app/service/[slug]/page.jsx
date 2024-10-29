3e7c247ae674ade95b1e3c344c8610ac1628fe91
Thanh <lygiathanh@gmail.com>
2024-10-29 21:56:24 +0700

    Rr

--- app/service/[slug]/page.jsx
+++ app/service/[slug]/page.jsx
@@ -1,19 +1,47 @@
 "use client";
 import { useParams } from "next/navigation";
+
+import { useState, useEffect } from "react";
 import { fetchServiceById } from "@/utils/requests";
 
-const page = async () => {
+const page = () => {
     const { slug } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [property, setProperty] = useState(null);
     console.log(slug);
-    const property = await fetchServiceById(slug);
+
+    useEffect(() => {
+        const fetchProperty = async () => {
+            if (!slug) return;
+            try {
+                const property = await fetchServiceById(slug);
+                setProperty(property);
+            } catch (error) {
+                console.error("Error fetching property", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        if (property === null) fetchProperty();
+    }, [slug, property]);
+
+    if (!property && !loading) {
+        return <h1>Not Found</h1>;
+    }
 
     return (
-        <div className="max-w-[1080px] lg:mx-auto mb-4 mx-3 ">
-            <h1 className="text-center text-xl font-bold my-4">
-                {property.title}
-            </h1>
-            <div dangerouslySetInnerHTML={{ __html: property.content }} />
-        </div>
+        <>
+            {!loading && property && (
+                <div className="max-w-[1080px] lg:mx-auto mb-4 mx-3 ">
+                    <h1 className="text-center text-xl font-bold my-4">
+                        {property.title}
+                    </h1>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: property.content }}
+                    />
+                </div>
+            )}
+        </>
     );
 };
 export default page;
